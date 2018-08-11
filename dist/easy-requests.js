21a37f57c83e6a6a000bb5c8d7dc53e6edb872a0
Cesar Santana <casc.santana@gmail.com>
2018-08-11 14:55:28 -0500

    Build for v2.0.0

--- dist/easy-requests.js
+++ dist/easy-requests.js
@@ -5,18 +5,18 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Model = exports.Trait = exports.Service = undefined;
 
-var _easyRequests = require('./src/easy-requests.js');
-
-var _easyRequests2 = _interopRequireDefault(_easyRequests);
-
 var _TraitBuilder = require('./Traits/TraitBuilder');
 
 var _Model = require('./Traits/Model.trait');
 
 var _Model2 = _interopRequireDefault(_Model);
 
+var _BaseService = require('./Base.service.js');
+
+var _BaseService2 = _interopRequireDefault(_BaseService);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.Service = _easyRequests2.default;
+exports.Service = _BaseService2.default;
 exports.Trait = _TraitBuilder.trait;
 exports.Model = _Model2.default;
